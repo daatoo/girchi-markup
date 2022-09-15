5ec1fa63da342ed1f9d85a3ff57acb8cbbecdcab
daatoo <davit.cheishvili.06@gmail.com>
2022-09-15 12:45:43 +0400

    step 2 page

--- girchi.js
+++ girchi.js
@@ -1,9 +1,14 @@
-let copytext = document.querySelector(".copy");
-copytext.querySelector("button").addEventListener("click", function(){
-    let input = copytext.querySelector("input.text");
-    input.ariaSelected();
-    document.execCommand("copy");
-    copytext.classList.add("active");
-    window.getSelection().removeAllRanges();
+// let copytext = document.querySelector(".copy");
+// copytext.querySelector("button").addEventListener("click", function(){
+//     let input = copytext.querySelector("input.text");
+//     input.ariaSelected();
+//     document.execCommand("copy");
+//     copytext.classList.add("active");
+//     window.getSelection().removeAllRanges();
     
-})
+// })
+    let cond = document.getElementById('name');
+    function isEmpty(cond){
+        return (cond === undefined || cond == null || cond.length <= 0) ? true : false;
+    };
+    console.log(cond)
